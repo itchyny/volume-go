81f6dce22430edad2f5c989538365ea4cca181cc
itchyny <itchyny@hatena.ne.jp>
2017-06-17 12:28:18 +0900

    implement volume for darwin

---
+++ volume_darwin.go
@@ -0,0 +1,18 @@
+package volume
+
+import (
+	"strconv"
+	"strings"
+)
+
+func getVolumeCmd() []string {
+	return []string{"osascript", "-e", "output volume of (get volume settings)"}
+}
+
+func parseVolume(out string) (int, error) {
+	return strconv.Atoi(strings.TrimSuffix(out, "\n"))
+}
+
+func setVolumeCmd(volume int) []string {
+	return []string{"osascript", "-e", "set volume output volume " + strconv.Itoa(volume)}
+}
