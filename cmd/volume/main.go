9b68129f055dfbc23964de2745dd05269cdd75eb
itchyny <itchyny@hatena.ne.jp>
2017-06-17 13:47:30 +0900

    implement volume command line tool

---
+++ cmd/volume/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ cmd/volume/run.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/itchyny/volume-go"
+)
+
+func run() error {
+	if len(os.Args) <= 1 {
+		return errors.New("no arg")
+	}
+	switch os.Args[1] {
+	case "get":
+		if len(os.Args) == 2 {
+			return getVolume()
+		}
+		return errors.New("invalid argument for volume get")
+	case "set":
+		if len(os.Args) == 3 {
+			return setVolume(os.Args[2])
+		}
+		return errors.New("invalid argument for volume get")
+	}
+	return fmt.Errorf("invalid argument for volume: %+v", os.Args[1:])
+}
+
+func getVolume() error {
+	vol, err := volume.GetVolume()
+	if err != nil {
+		return err
+	}
+	fmt.Println(vol)
+	return nil
+}
+
+func setVolume(volStr string) error {
+	vol, err := strconv.Atoi(volStr)
+	if err != nil {
+		return err
+	}
+	return volume.SetVolume(vol)
+}
