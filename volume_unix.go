ece76317ca98d5170169ca2184e108308088decd
itchyny <itchyny@hatena.ne.jp>
2017-06-17 12:32:26 +0900

    implement volume for unix

---
+++ volume_unix.go
@@ -0,0 +1,32 @@
+// +build linux freebsd netbsd
+
+package volume
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func getVolumeCmd() []string {
+	return []string{"pactl", "list", "sinks"}
+}
+
+var volumePattern = regexp.MustCompile(`\d+%`)
+
+func parseVolume(out string) (int, error) {
+	lines := strings.Split(out, "\n")
+	for _, line := range lines {
+		s := strings.TrimLeft(line, " \t")
+		if strings.HasPrefix(s, "Volume: 0:") {
+			volumeStr := volumePattern.FindString(s)
+			return strconv.Atoi(volumeStr[:len(volumeStr)-1])
+		}
+	}
+	return 0, errors.New("no volume found")
+}
+
+func setVolumeCmd(volume int) []string {
+	return []string{"pactl", "set-sink-volume", "0", strconv.Itoa(volume) + "%"}
+}
